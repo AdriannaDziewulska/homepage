28f9dbbd57d29ba9defc4c6314db04365124bfc5
Adrianna Dziewulska <dziewulska37@gmail.com>
2020-01-27 19:28:38 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,1 @@
+console.log(`Cześć! Jestem autorką tej strony, nazywam się Ada Dziewulska i mam 14 lat, miło, że tu zglądasz, a skoro to robisz to trochę się interesujesz JavaScript-em ;) Ja się już żegnam i życzę miłego dnia :D`); 
