ffc07102bdd4370dae4a5563561cac226fd0b554
Adrianna Dziewulska <dziewulska37@gmail.com>
2020-02-18 11:29:38 +0100

    welcome

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-console.log(`Cześć! Jestem autorką tej strony, nazywam się Ada Dziewulska i mam 14 lat, miło, że tu zglądasz, a skoro to robisz to trochę się interesujesz JavaScript-em ;) Ja się już żegnam i życzę miłego dnia :D`); 
+console.log(`Cześć! Jestem autorką tej strony, nazywam się Ada Dziewulska i mam 15 lat, miło, że tu zglądasz, a skoro to robisz to trochę się interesujesz JavaScript-em ;) Ja się już żegnam i życzę miłego dnia :D`); 
 
-const section = document.querySelector ('section')
-console.log (section)
-const paragrahps = document.querySelectorAll ('p')
-console.log (paragrahps)
+const welcome = (name, age) => {
+    console.log(`Witaj ${name}, masz ${age} lat.`)
+}
+welcome(`Ada`, 15)
